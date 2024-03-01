3d8b3a69add76dba879547f62652fbc17b7a44d1
FMABrotherhoodX <31969916+FMABrotherhoodX@users.noreply.github.com>
2024-03-01 12:38:38 -0500

    redadded main file and updated test widget
    reupdated widget.test because main dart was need to run program

---
+++ recipe_mealplanning/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'home_screen.dart';
+import 'recipe_provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => RecipeProvider(),
+      child: MaterialApp(
+        title: 'Meal Planner',
+        theme: ThemeData(
+          primarySwatch: Colors.amber,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: const HomeScreen(),
+      ),
+    );
+  }
+}
