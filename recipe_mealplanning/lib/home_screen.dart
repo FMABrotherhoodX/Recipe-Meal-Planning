0314c01a7839be986758152703d763eff65866e9
FMABrotherhoodX <31969916+FMABrotherhoodX@users.noreply.github.com>
2024-03-01 12:23:16 -0500

    updated home screen and  reicpe list screen
    updated home screen to make connection with recipe list screen which will help display and view recipes

--- recipe_mealplanning/lib/home_screen.dart
+++ recipe_mealplanning/lib/home_screen.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
-import '';
+import 'recipe_list_screen.dart';
 
 class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Meal Planner'),
+        title: const Text('Meal Planner'),
       ),
       body: Center(
         child: ElevatedButton(
-          onPressed: () => Navigator.push(context,
-              MaterialPageRoute(builder: (context) => recipelistscreen())),
-          child: Text('View Recipe'),
+          onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => const RecipeListScreen())),
+          child: const Text('View Recipe'),
         ),
       ),
     );
--- recipe_mealplanning/lib/recipe_list_screen.dart
+++ recipe_mealplanning/lib/recipe_list_screen.dart
@@ -5,15 +5,28 @@ import 'recipe_provider.dart';
 import 'recipe_card.dart';
 
 class RecipeListScreen extends StatelessWidget {
+  const RecipeListScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     List<Recipe> recipes = Provider.of<RecipeProvider>(context).recipes;
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Recipes'),
+        title: const Text('Recipes'),
       ),
-      body: ListView.builder(),
+      body: ListView.builder(
+        itemCount: recipes.length,
+        itemBuilder: (context, index) {
+          final recipe = recipes[index];
+          return RecipeCard(recipe: recipe);
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Add Recipe',
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
