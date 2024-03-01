c1a713727762e2a25ac734f765757e72d5f4481e
FMABrotherhoodX <31969916+FMABrotherhoodX@users.noreply.github.com>
2024-03-01 18:27:32 -0500

    Added a delete function for recipe
    added a delete function for the program

--- recipe_mealplanning/lib/Screens/recipe_detail_screen.dart
+++ recipe_mealplanning/lib/Screens/recipe_detail_screen.dart
@@ -1,16 +1,62 @@
 import 'package:flutter/material.dart';
-import '../recipe_model.dart'; // Import your Recipe model
+import 'package:provider/provider.dart';
+import 'package:recipe_mealplanning/recipe_model.dart';
+import 'package:recipe_mealplanning/recipe_provider.dart';
 
 class RecipeDetailScreen extends StatelessWidget {
   final Recipe recipe;
 
   const RecipeDetailScreen({Key? key, required this.recipe}) : super(key: key);
 
+  Future<void> _showDeleteDialog(BuildContext context) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // User must tap button to close the dialog
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Delete Recipe'),
+          content: const SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('Are you sure you want to delete this recipe?'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+            ),
+            TextButton(
+              child: const Text('Delete'),
+              onPressed: () {
+                // Call the delete function from your provider here
+                Provider.of<RecipeProvider>(context, listen: false)
+                    .deleteRecipe(recipe.id);
+                Navigator.of(context).pop(); // Close the dialog
+                Navigator.of(context)
+                    .pop(); // Go back to the previous screen, recipe list screen
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(recipe.name), // Display the recipe name in the app bar
+        title: Text(recipe.name),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.delete),
+            onPressed: () => _showDeleteDialog(context),
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -22,14 +68,14 @@ class RecipeDetailScreen extends StatelessWidget {
               padding: const EdgeInsets.all(8.0),
               child: Text(
                 recipe.name,
-                style: Theme.of(context).textTheme.headline6,
+                style: Theme.of(context).textTheme.titleLarge,
               ),
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Text(
                 'Ingredients',
-                style: Theme.of(context).textTheme.subtitle1,
+                style: Theme.of(context).textTheme.titleMedium,
               ),
             ),
             Padding(
@@ -40,7 +86,7 @@ class RecipeDetailScreen extends StatelessWidget {
               padding: const EdgeInsets.all(8.0),
               child: Text(
                 'Description',
-                style: Theme.of(context).textTheme.subtitle1,
+                style: Theme.of(context).textTheme.titleMedium,
               ),
             ),
             Padding(
